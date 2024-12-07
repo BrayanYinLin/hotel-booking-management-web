d350e03bcb90f97d45bec5b08fca72b1d698d93d
Brayan Yin Lin <brayanyl250206@gmail.com>
2024-12-07 04:46:40 -0500

    feat: separacion en N capas

---
+++ ProyHotel_BL/UsuarioBL.cs
@@ -0,0 +1,20 @@
+﻿using ProyHotel_ADO;
+using ProyHotel_BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyHotel_BL
+{
+    public class UsuarioBL
+    {
+        UsuarioADO usuarioADO = new UsuarioADO();
+
+        public UsuarioBE IniciarSesion(UsuarioBE usuarioBE)
+        {
+            return usuarioADO.IniciarSesion(usuarioBE);
+        }
+    }
+}
--- hotel-booking-management/Login.aspx.cs
+++ hotel-booking-management/Login.aspx.cs
@@ -5,12 +5,16 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using hotel_booking_management;
+using ProyHotel_BE;
+using ProyHotel_BL;
 
 namespace DemoWEB_Sem10
 {
     public partial class Login : System.Web.UI.Page
     {
-        hotel_databaseEntities hotelDB = new hotel_databaseEntities();
+        UsuarioBL usuarioBL = new UsuarioBL();
+        UsuarioBE usuarioBE = new UsuarioBE();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             ErrorMessage.Text = String.Empty;
@@ -18,32 +22,23 @@ namespace DemoWEB_Sem10
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-
             try
             {
-                //Codifique
-                string username = txtUsername.Text;
-                string password = txtPassword.Text;
+                UsuarioBE usuarioRegistrado = new UsuarioBE();
+                usuarioRegistrado.usuarioNombre = txtUsername.Text;
+                usuarioRegistrado.usuarioClave = txtPassword.Text;
 
-                var query = hotelDB.tb_usuario.ToList();
-
-                foreach (var item in query)
+                usuarioBE = usuarioBL.IniciarSesion(usuarioRegistrado);
+                if (usuarioBE != null)
                 {
-                    if (item.usuario_nombre == username && item.usuario_password == password)
-                    {
-                        Response.Redirect("Inicio.aspx");
-                        return;
-                    }
+                    Response.Redirect("Inicio.aspx");
+                    return;
                 }
-
-                throw new Exception("Usuario y/o contraseña incorrecta.");
             }
             catch (Exception ex)
             {
-
                 ErrorMessage.Text = "<div class='alert alert-danger' role='alert'>" + ex.Message + "</div>";
             }
-
         }
     }
 }
